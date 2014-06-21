86fa6fcb04f33fad247497d93fcfc1f285138260
Alex Robertson <alex@someoddpilot.com>
2014-06-21 18:51:20 -0500

    Step 13

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function clickButton(message) {
     alert(message);
 }
+
+var button = document.querySelector('.click-me');
+
+button.addEventListener('click', function () {
+    clickButton('I was clicked using an event handler!');
+});
